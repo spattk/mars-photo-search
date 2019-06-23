c0fd58ffdea11881d87da36ac499b53d35f050fd
Sitesh Pattanaik <spattanaik@yodlee.com>
2019-06-23 22:03:16 +0530

    Added React-Bootstrap and completed the components

--- client/src/App/pages/Home.js
+++ client/src/App/pages/Home.js
@@ -1,44 +1,52 @@
 import React, { Component } from 'react';
 import Search from '../../components/Search'
 import Header from '../../components/layout/Header'
+import ImageItem from '../../components/ImageItem';
+import Container from 'react-bootstrap/Container';
 
 class Home extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            list: []
+            list: [],
+            API_KEY : '0ywd5r3RzqvCZoo5Y9ZZZHRVfklCUbfOR859tIlP',
+            isFirst : true
         }
     }
 
-    componentDidMount() {
-        this.getList();
+    onSearchClick = (msoi,camera) => {
+        //console.log(`from home.js ${msoi} and ${camera}`);
+        fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${msoi}&camera=${camera}&api_key=${this.state.API_KEY}`)
+        .then(res => res.json())
+        .then(photos => this.setState({list : photos.photos, isFirst: false}))
     }
 
-    getList = () => {
-        fetch('/api/trial')
-            .then(res => res.json())
-            .then(list => this.setState({ list }))
-    }
 
     render() {
-        const { list } = this.state;
+        const { list, isFirst } = this.state;
         return (
-            <div>
+            <div className='App'>
                 <Header />
-                <Search />
-                <h1>List of Items</h1>
+                <br />
+                <Container>
+                <Search onSearchClick={this.onSearchClick} results={this.state.list}/>
                 {
+                    
                     list.length ?
                         (list.map((item) => {
                             return (
-                                <div>{item}</div>
+                                <ImageItem key={item.id} img={item.img_src} ></ImageItem>
                             )
                         })) :
-                        (<div>
-                            <h2>No List Items Found</h2>
-                        </div>)
-                }
+                        (
+                            isFirst ? 
+                            (<Container><br/><div ><h6>Please start your searching</h6></div></Container>) : (<Container>
+                            <br/><div>
+                            <h6 style={{textTransform:'uppercase'}}>No photos found for the search !!!</h6>
+                        </div></Container>))
+                }   
+                </Container>             
             </div>
         )
     }
