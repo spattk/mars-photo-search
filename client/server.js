604c0ab4e1a29bf39acb1fb5b33e1b37879b21ee
Sitesh Pattanaik <spattanaik@yodlee.com>
2019-06-24 17:30:30 +0530

    Heroku specific changes

--- client/server.js
+++ client/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const app = express();
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
