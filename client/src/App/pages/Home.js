9d645b4228b879d96d5cd46cdc0ced5e1a60aba6
Sitesh Pattanaik <spattanaik@yodlee.com>
2019-06-24 12:41:43 +0530

    On incorrect search cleaned the list and handled loading for search button

--- client/src/App/pages/Home.js
+++ client/src/App/pages/Home.js
@@ -10,16 +10,20 @@ class Home extends Component {
         super(props);
         this.state = {
             list: [],
-            API_KEY : '0ywd5r3RzqvCZoo5Y9ZZZHRVfklCUbfOR859tIlP',
-            isFirst : true
+            API_KEY: '0ywd5r3RzqvCZoo5Y9ZZZHRVfklCUbfOR859tIlP',
+            isFirst: true
         }
     }
 
-    onSearchClick = (msoi,camera) => {
+    onSearchClick = (msoi, camera) => {
         //console.log(`from home.js ${msoi} and ${camera}`);
         fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${msoi}&camera=${camera}&api_key=${this.state.API_KEY}`)
-        .then(res => res.json())
-        .then(photos => this.setState({list : photos.photos, isFirst: false}))
+            .then(res => res.json())
+            .then(photos => this.setState({ list: photos.photos, isFirst: false }))
+    }
+
+    clearList = () => {
+        this.setState({ list: [] })
     }
 
 
@@ -30,23 +34,23 @@ class Home extends Component {
                 <Header />
                 <br />
                 <Container>
-                <Search onSearchClick={this.onSearchClick} results={this.state.list}/>
-                {
-                    
-                    list.length ?
-                        (list.map((item) => {
-                            return (
-                                <ImageItem key={item.id} img={item.img_src} ></ImageItem>
-                            )
-                        })) :
-                        (
-                            isFirst ? 
-                            (<Container><br/><div ><h6>Please start your searching</h6></div></Container>) : (<Container>
-                            <br/><div>
-                            <h6 style={{textTransform:'uppercase'}}>No photos found for the search !!!</h6>
-                        </div></Container>))
-                }   
-                </Container>             
+                    <Search onSearchClick={this.onSearchClick} results={this.state.list} clearList={this.clearList} />
+                    {
+
+                        list.length ?
+                            (list.map((item) => {
+                                return (
+                                    <ImageItem key={item.id} img={item.img_src} ></ImageItem>
+                                )
+                            })) :
+                            (
+                                isFirst ?
+                                    (<Container><br /><div ><h6>Please start your searching</h6></div></Container>) : (<Container>
+                                        <br /><div>
+                                            <h6 style={{ textTransform: 'uppercase' }}>No photos found for the search !!!</h6>
+                                        </div></Container>))
+                    }
+                </Container>
             </div>
         )
     }
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -6,24 +6,31 @@ import Row from 'react-bootstrap/Row'
 import Dropdown from 'react-bootstrap/Dropdown'
 import Col from 'react-bootstrap/Col'
 
-class Search extends Component {
+function simulateNetworkRequest() {
+  return new Promise(resolve => setTimeout(resolve, 2000));
+}
 
+class Search extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
       msoi: '',
       camera: '',
-      hasError: false
+      hasError: false,
+      btnContent: 'Find Photos'
     }
   }
 
   onSearchClick = (msoi, camera) => {
-    if(msoi === '' || camera === ''){
-      this.setState({hasError : true})
+    if (msoi === '' || camera === '') {
+      this.setState({ hasError: true });
+      this.props.clearList();
     }
-    else{
-      this.setState({hasError:false})
+    else {
+      this.setState({ hasError: false, btnContent: 'Loading...' }, () => {
+        simulateNetworkRequest().then(() => { this.setState({ btnContent: 'Photo Search' }) })
+      })
       this.props.onSearchClick(msoi, camera);
     }
   }
@@ -38,72 +45,69 @@ class Search extends Component {
 
   render() {
 
-    const lists = this.props.results.map((result)=>{
+    const lists = this.props.results.map((result) => {
       return (<div key={result.id}>{result.body}</div>)
     })
 
     return (
       <React.Fragment>
-      <Form>
-        <Container>
-                   
-          <Row>
-            <Col className='col-md-5 '>
-              <Form.Group as={Row} controlId="formPlaintextEmail">
-                <Form.Label column md={4} >
-                
-                  Mission SoI <br /> <span style={{color:'red', fontWeight:'bold'}}> Integer > 0 </span>
-                  
-                </Form.Label>
-                <Col>
-                  <Form.Control required type="number" name="msoi" value={this.state.msoi} onChange={this.onChange} />
-                </Col>
-              </Form.Group>
-            </Col>
+        <Form>
+          <Container>
 
-            <Col className='col-md-5'>
-              <Form.Group as={Row} controlId="formBasicPassword" >
-                <Form.Label column md={2}>Camera</Form.Label>
-                <Col>
-                  <Dropdown>
-                    <Dropdown.Toggle variant="primary" id="dropdown-basic">
-                      {this.state.camera}
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu value={this.state.camera} required>
-                      <Dropdown.Item onClick={this.onSelect}>FHAZ</Dropdown.Item>
-                      <Dropdown.Item onClick={this.onSelect}>RHAZ</Dropdown.Item>
-                      <Dropdown.Item onClick={this.onSelect}>MAST</Dropdown.Item>
-                      <Dropdown.Item onClick={this.onSelect}>CHEMCAM</Dropdown.Item>
-                      <Dropdown.Item onClick={this.onSelect}>MAHLI</Dropdown.Item>
-                      <Dropdown.Item onClick={this.onSelect}>MARDI</Dropdown.Item>
-                      <Dropdown.Item onClick={this.onSelect}>NAVCAM</Dropdown.Item>
-                      <Dropdown.Item onClick={this.onSelect}>PANCAM</Dropdown.Item>
-                      <Dropdown.Item onClick={this.onSelect}>MINITES</Dropdown.Item>
+            <Row>
+              <Col className='col-md-5 '>
+                <Form.Group as={Row} controlId="formPlaintextEmail">
+                  <Form.Label column md={4} >
 
-                    </Dropdown.Menu>
-                  </Dropdown>
-                </Col>
-              </Form.Group>
-            </Col>
+                    Mission SoI <br /> <span style={{ color: 'red', fontWeight: 'bold' }}> Integer > 0 </span>
 
-            <Col className='col-md-2'>
-              <Button onClick={()=>{this.onSearchClick(this.state.msoi,this.state.camera)}}>
-                Find Photos
-              </Button>
-            </Col>
-          </Row>
+                  </Form.Label>
+                  <Col>
+                    <Form.Control required type="number" name="msoi" value={this.state.msoi} onChange={this.onChange} />
+                  </Col>
+                </Form.Group>
+              </Col>
 
-          
+              <Col className='col-md-5'>
+                <Form.Group as={Row} controlId="formBasicPassword" >
+                  <Form.Label column md={2}>Camera</Form.Label>
+                  <Col>
+                    <Dropdown>
+                      <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                        {this.state.camera}
+                      </Dropdown.Toggle>
+                      <Dropdown.Menu value={this.state.camera} required>
+                        <Dropdown.Item onClick={this.onSelect}>FHAZ</Dropdown.Item>
+                        <Dropdown.Item onClick={this.onSelect}>RHAZ</Dropdown.Item>
+                        <Dropdown.Item onClick={this.onSelect}>MAST</Dropdown.Item>
+                        <Dropdown.Item onClick={this.onSelect}>CHEMCAM</Dropdown.Item>
+                        <Dropdown.Item onClick={this.onSelect}>MAHLI</Dropdown.Item>
+                        <Dropdown.Item onClick={this.onSelect}>MARDI</Dropdown.Item>
+                        <Dropdown.Item onClick={this.onSelect}>NAVCAM</Dropdown.Item>
+                        <Dropdown.Item onClick={this.onSelect}>PANCAM</Dropdown.Item>
+                        <Dropdown.Item onClick={this.onSelect}>MINITES</Dropdown.Item>
 
-          <Row>
-            <Col>
-            {this.state.hasError ? (<div style={{color:'red'}}>Both the fields are required. Please input the values and then continue your search<br />Ex : Mission SoI : 100, Camera : FHAZ</div>) : (<div>{lists}</div>)}
-            </Col>
-          </Row>
-        </Container>
-      </Form>
+                      </Dropdown.Menu>
+                    </Dropdown>
+                  </Col>
+                </Form.Group>
+              </Col>
 
-      <div ></div>
+              <Col className='col-md-2'>
+                <Button onClick={() => { this.onSearchClick(this.state.msoi, this.state.camera) }}>
+                  {this.state.btnContent}
+                </Button>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col>
+                {this.state.hasError ? (<div style={{ color: 'red' }}>Both the fields are required. Please input the values and then continue your search<br />Ex : Mission SoI : 100, Camera : FHAZ</div>) : (<div>huhu{lists}</div>)}
+              </Col>
+            </Row>
+          </Container>
+        </Form>
+
       </React.Fragment>
     )
   }
